caec68405474b3d4f16ae1478e7094e5375dc1ae
Matheus Morais <matheus_morais_martins@outlook.com>
2026-08-18 13:46:21 -0300

    Implementa modal de login do cliente e navegacao para o ccatalogo

---
+++ js/app.js
@@ -0,0 +1,66 @@
+// Botao responsavel por abrir a lista de clientes
+const botaoSelecionarCliente = document.querySelector(".btn-selecionar-cliente");
+
+//Lista que contem os clientes cadastrados
+const listaClientes = document.querySelector(".lista-clientes");
+
+//Botao "Entrar como cliente da tela inicial"
+const botaoCliente = document.querySelector(".btn-cliente");
+
+//Botao X do modal
+const botaoFecharModal = document.querySelector(".btn-fechar-modal");
+
+//Modal de login
+const modalLogin = document.querySelector(".modal-login");
+
+//otao para entrar apos sleecionar um cliente
+const botaoEntrarCliente = document.querySelector(".btn-entrar-cliente");
+
+//Executa uma açao quando o usuario clicar no seletor de cliente
+botaoSelecionarCliente.addEventListener("click", function (){
+
+    if (listaClientes.style.display === "flex"){
+        listaClientes.style.display = "none";
+    } else{
+        listaClientes.style.display = "flex";
+        listaClientes.scrollTop = 0; //faz com que a lista sempre abra no topo
+    }
+
+});
+
+//Botoes de clientes disponiveis na lista
+const botoesClientes = document.querySelectorAll(".lista-clientes button");
+
+//percorre todos os botoes de cliuentes
+botoesClientes.forEach(function (botaoCliente){
+
+    //quando clicar em um cliente
+    botaoCliente.addEventListener("click", function(){
+
+        //troca o texto do seletor pelo nome do cliente escolhido
+        botaoSelecionarCliente.innerHTML =
+            botaoCliente.textContent + ' <i class="bi bi-chevron-down"></i>';
+
+        //fechar a lista depois que selecionar um cliente
+        listaClientes.style.display = "none";
+
+        //Mostra o botao entrar
+        botaoEntrarCliente.style.display = "block";
+
+    });
+});
+
+//Abre o modal ao clicar em "Entrar como Cliente"
+botaoCliente.addEventListener("click", function(){
+    modalLogin.style.display = "flex";
+
+});
+
+//fechar o modal clicando no X
+botaoFecharModal.addEventListener("click", function(){
+    modalLogin.style.display = "none";
+})
+
+botaoEntrarCliente.addEventListener("click", function(){
+    window.location.href = "cliente/catalogo.html";
+});
