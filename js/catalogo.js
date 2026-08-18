06b4d6a754f4c06f3456d46565ebd210add767c1
Matheus Morais <matheus_morais_martins@outlook.com>
2026-08-18 20:48:11 -0300

    Adiciona a pagina de catalogo e o modal de ver detalhes nos livro

---
+++ js/catalogo.js
@@ -0,0 +1,27 @@
+//Seleciona todos os botoes "Ver detalhes"
+const botoesDetalhes = document.querySelectorAll(".btn-detalhes");
+
+//Seleciona o modal
+const modalLivro = document.querySelector(".modal-livro");
+
+//Seleciona o botao X
+const botaoFecharLivro = document.querySelector(".btn-fechar-livro");
+
+const botaoAdicionarCarrinho = document.querySelector(".btn-adicionar-carrinho");
+
+//Abre o modao ao clicar em qualquer botao "ver detalhes"
+botoesDetalhes.forEach(function (botao){
+
+    botao.addEventListener("click", function (){
+        modalLivro.style.display = "flex";
+    });
+});
+
+//Fechar o modal quando clic no X
+botaoFecharLivro.addEventListener("click", function(){
+    modalLivro.style.display = "none";
+});
+
+botaoAdicionarCarrinho.addEventListener("click", function (){
+    alert("Livro adicionado ao carrinho!");
+});
