15de46d1e5a44b4be54017ae80ccfd382cf2cb9e
Matheus Morais <matheus_morais_martins@outlook.com>
2026-08-23 19:23:42 -0300

    Implementa acoes e detalhes dos pedidos

--- js/pedidos.js
+++ js/pedidos.js
@@ -2,6 +2,16 @@ const botaoCancelarPedido = document.querySelector(".btn-cancelar-pedido");
 
 const botaoConfirmarRecebimento = document.querySelector(".btn-confirmar-recebimento");
 
+const botaoSolicitarTroca = document.querySelector(".btn-solicitar-troca");
+
+const formularioTroca = document.querySelector(".form-troca");
+
+const botaoEnviarTroca = document.querySelector(".btn-enviar-troca");
+
+const botaoInformarDespacho = document.querySelector(".btn-informar-despacho");
+
+const botoesVerDetalhes = document.querySelectorAll(".btn-ver-detalhes");
+
 botaoCancelarPedido.addEventListener("click", function(){
 
     const pedido = botaoCancelarPedido.closest(".pedido");//procura um elemtno em ".pedido" que tenha sido usado anteriormente
@@ -25,4 +35,54 @@ botaoConfirmarRecebimento.addEventListener("click", function(){
     botaoConfirmarRecebimento.style.display = "none";
 
     alert("Recebimento confirmado com sucesso!");
+});
+
+botaoSolicitarTroca.addEventListener("click", function(){
+    formularioTroca.style.display = "block";
+});
+
+botaoEnviarTroca.addEventListener("click", function(){
+    
+    const pedido = botaoSolicitarTroca.closest(".pedido");
+
+    const statusPedido = pedido.querySelector(".status-pedido");
+    
+    statusPedido.textContent = "Troca solicitada";
+
+    formularioTroca.style.display = "none";
+
+    botaoSolicitarTroca.style.display = "none";
+
+    alert("Solicitação de troca enviada com sucesso!");
+});
+
+botaoInformarDespacho.addEventListener("click", function(){
+
+    const pedido = botaoInformarDespacho.closest(".pedido");
+
+    const statusPedido = pedido.querySelector(".status-pedido");
+
+    statusPedido.textContent = "item enviado";
+
+    botaoInformarDespacho.style.display = "none";
+
+    alert("Despacho do item informado com sucesso!");
+});
+
+botoesVerDetalhes.forEach(function (botao){
+
+    botao.addEventListener("click", function(){
+
+        const pedido = botao.closest(".pedido");
+
+        const detalhesPedido = pedido.querySelector(".detalhes-pedido");
+
+        if (detalhesPedido.style.display === "none"){
+            detalhesPedido.style.display = "block";
+            botao.textContent = "Ocultar detalhes";
+        } else{
+            detalhesPedido.style.display = "none";
+            botao.textContent = "Ver detalhes";
+        }
+    });
 });
