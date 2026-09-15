ba316feb74603ba7841cc956ed58a9d36586e0fc
Matheus Morais <matheus_morais_martins@outlook.com>
2026-09-15 12:57:24 -0300

    Configura backend Spring Boot e PostgreSQL

---
+++ backend/src/main/java/br/com/elivros/ElivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.elivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElivrosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/elivros/ElivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
